3e8bb6ae91e48c3ec598e5a6922b1a7ccd8c98a7
ShreyaPatel20063 <sap20063@gmailcom>
2022-08-09 13:59:42 +0530

    4 frames made

--- src/Login_SignUp.java
+++ src/Login_SignUp.java
@@ -4,15 +4,14 @@ import java.awt.*;          //awt = abstract windowing toolkit
 /*
 * JFrame uses Card Layout by default
 * It replaces initial add() function and overwrites it*/
-public class Login_SignUp extends JFrame{              //GUI for login page
-    JFrame lgsg_Frame;
+class Login_SignUp extends JFrame{              //GUI for login page
     Login_SignUp(){          // Constructor with JFrame methods
-        lgsg_Frame = new JFrame("ATM Walk-through Project");
-        //lgsg_Frame.setLayout(new GridBagLayout());
-
-        lgsg_Frame.setBounds(300, 300, 350, 350);
+        JFrame lgsg_Frame = new JFrame("ATM Walk-through Project");
         lgsg_Frame.setVisible(true);
-        lgsg_Frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        lgsg_Frame.setLayout(new FlowLayout());
+        lgsg_Frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        lgsg_Frame.setBounds(300, 300, 300, 250);
+
 
         JLabel Card_label = new JLabel("Card Number : ");
         JLabel Pswrd_label = new JLabel("Password : ");
@@ -21,6 +20,7 @@ public class Login_SignUp extends JFrame{              //GUI for login page
         JPasswordField Pswrd_Text = new JPasswordField(20);
         JPasswordField CVV_Text = new JPasswordField(3);
 
+
         lgsg_Frame.add(Card_label);
         lgsg_Frame.add(Card_text);
         lgsg_Frame.add(Pswrd_label);
@@ -28,10 +28,12 @@ public class Login_SignUp extends JFrame{              //GUI for login page
         lgsg_Frame.add(CVV_label);
         lgsg_Frame.add(CVV_Text);
         JButton login_button = new JButton("Log In");        //create login button
-        // login_button.setBounds(320, 400, 20, 7);
+        login_button.setBounds(320, 400, 20, 7);
         lgsg_Frame.add(login_button);                               //add login button
 
+
     }      //Constructor which creates frame of Login box
+
 
     public void FrameSetup(){
         setLayout(new FlowLayout());
@@ -45,9 +47,58 @@ public class Login_SignUp extends JFrame{              //GUI for login page
     }
 }
 
-class Login extends Login_SignUp{
+class Login extends JFrame{
     public Login() {
-        super();
+        JFrame login_page = new JFrame("Edit Money");
+        login_page.setVisible(true);
+        login_page.setLayout(new FlowLayout());
+        login_page.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        login_page.setBounds(350, 350, 250, 250);
+
+        JButton Withdraw = new JButton("Withdraw Money");
+        JButton CurrentBal = new JButton("Current Balance");
+        JButton Deposit = new JButton("Deposit Money");
+
+        login_page.add(Withdraw);
+        login_page.add(Deposit);
+        login_page.add(CurrentBal);
     }
 }
+class WithDraw extends JFrame{
+    WithDraw(){
+        JFrame withdraw_frame = new JFrame("Withdraw your Money");
+        withdraw_frame.setVisible(true);
+        withdraw_frame.setLayout(new FlowLayout());
+        withdraw_frame.setBounds(300,300,300,300);
+        withdraw_frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        withdraw_frame.add(new JLabel("Total Amount to Withdraw:"));
+
+        JTextField withdraw_money = new JTextField("Enter Amount");
+        withdraw_frame.add(withdraw_money);
+
+        JButton wd_button = new JButton("Withdraw");
+        withdraw_frame.add(wd_button);
+
+    }
+}
+
+class Deposit extends JFrame{
+    Deposit(){
+        JFrame deposit_frame = new JFrame("Deposit your Money");
+        deposit_frame.setVisible(true);
+        deposit_frame.setLayout(new FlowLayout());
+        deposit_frame.setBounds(300,300,300,300);
+        deposit_frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        deposit_frame.add(new JLabel("Total Amount to Deposit:"));
+
+        JTextField deposit_money = new JTextField("Enter Amount");
+        deposit_frame.add(deposit_money);
+
+        JButton dep_button = new JButton("Deposit");
+        deposit_frame.add(dep_button);
+    }
+}
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import MoneyMatter.*;
+import AccountDetails.*;
 public class Main {
     public static void main(String[] args) {
-        //Login_SignUp LS = new Login_SignUp();
+        Login_SignUp LS = new Login_SignUp();
         Login lg = new Login();
-    }
+        WithDraw wd = new WithDraw();
+        Deposit dep = new Deposit();
+}
 }
