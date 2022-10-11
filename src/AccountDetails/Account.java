8cf0088938519c5c6d5893c4715c291fc76a6e5f
ShreyaPatel20063 <sap20063@gmail.com>
2022-10-12 00:02:32 +0530

    ArrayList added

--- src/AccountDetails/Account.java
+++ src/AccountDetails/Account.java
@@ -1,6 +1,77 @@
 package AccountDetails;     //what is your acc details - acc no, name , password
 import java.io.*;
+import java.util.*;
 
 public class Account {
+    Scanner sc = new Scanner(System.in);
+    //Adding details to check in Login_SignUp form
+    ArrayList<ArrayList<Object>> accdetails = new ArrayList<>(5);
+    public Account(){
 
+        /** 2D ArrayList
+         * CardNumber, Password, CVV */
+
+
+        for (int i = 0; i < 3; i++) {
+            accdetails.add(new ArrayList<>());
+        }
+
+        accdetails.get(1).add("123412341234");
+        accdetails.get(1).add("2006");
+        accdetails.get(1).add("123");
+
+        accdetails.get(2).add("456745674567");
+        accdetails.get(2).add("2407");
+        accdetails.get(2).add("232");
+
+        accdetails.get(0).add("369369369369");
+        accdetails.get(0).add("0910");
+        accdetails.get(0).add("143");
+
+
+    }
+
+    Object setCno(String cno){
+        return cno;
+    }
+    Object setPswrd(String pswrd){
+        return pswrd;
+    }
+    Object setCvv(String cvv){
+        return cvv;
+    }
+
+    public int index(String cno){
+        int flag =-1;
+        for (int i = 0; i < 3; i++) {
+            if(accdetails.get(i).contains(setCno(cno))){
+                flag =i;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    public boolean containAc(String cno, String pswrd, String cvv){
+        int index = index(cno);
+        //System.out.println(index);
+        if(index >=0){
+            //System.out.println(index);
+            Object[] acc = new Object[3];
+            acc = accdetails.get(index).toArray();
+            if(acc[1].equals(setPswrd(pswrd))  && acc[2].equals(setCvv(cvv))){
+                return true;
+            }else{
+                System.out.println("Pswrd/CVV does not match!!");
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    /*public static void main(String[] args) {
+        Account ac = new Account();
+        System.out.println(ac.containAc("123412341234", "2006", "123"));
+    }*/
 }
--- src/Login_SignUp.java
+++ src/Login_SignUp.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;          //awt = abstract windowing toolkit
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import AccountDetails.*;
 
 /*
 * JFrame uses Card Layout by default
@@ -19,46 +20,62 @@ class Login_SignUp extends JFrame{
     JButton login_button = new JButton("Log In");        //create login button
 
     Login_SignUp(){          // Constructor with JFrame methods
-        //JFrame lgsg_Frame = new JFrame("ATM Walk-through Project");
+
+        //frame created
         lgsg_Frame.setVisible(true);
         lgsg_Frame.setLayout(new FlowLayout());
         lgsg_Frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         lgsg_Frame.setBounds(300, 300, 300, 250);
 
-
-        /*JLabel Card_label = new JLabel("Card Number : ");
-        JLabel Pswrd_label = new JLabel("Password : ");
-        JLabel CVV_label = new JLabel("CVV : ");
-        JTextField Card_text = new JTextField(12);
-        JPasswordField Pswrd_Text = new JPasswordField(20);
-        JPasswordField CVV_Text = new JPasswordField(3);*/
-
-
+        // operations added to frame
         lgsg_Frame.add(Card_label);
         lgsg_Frame.add(Card_text);
         lgsg_Frame.add(Pswrd_label);
         lgsg_Frame.add(Pswrd_Text);
         lgsg_Frame.add(CVV_label);
         lgsg_Frame.add(CVV_Text);
-        //JButton login_button = new JButton("Log In");        //create login button
         login_button.setBounds(320, 400, 20, 7);
         lgsg_Frame.add(login_button);                               //add login button
 
         LoginActionListener();
 
+
     }      //Constructor which creates frame of Login box
 
     public void LoginActionListener(){
-        login_button.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                //JLabel txt = new JLabel("Clicked");
-                Login lg = new Login();
-                lg.setVisible(true);
-                lgsg_Frame.setVisible(false);
 
-            }
-        });
+            login_button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (detailsValidation()){
+                        //JLabel txt = new JLabel("Clicked");
+                        Login lg = new Login();
+                        lg.setVisible(true);
+                        lgsg_Frame.setVisible(false);
+                    }
+                }
+            });
+
     }
+
+    boolean detailsValidation(){
+        String cno, cvv;
+        cno = Card_text.getText();
+        cvv = String.valueOf(CVV_Text.getPassword());
+        String pswrd = String.valueOf(Pswrd_Text.getPassword());
+
+        JLabel txt = new JLabel(cno);
+        JLabel txt1 = new JLabel(cvv);
+        JLabel txt2 = new JLabel(pswrd);
+        lgsg_Frame.add(txt);
+        lgsg_Frame.add(txt1);
+        lgsg_Frame.add(txt2);
+
+        Account ac = new Account();
+        return ac.containAc(cno, pswrd, cvv);
+    }
+
+
 }
 
 
